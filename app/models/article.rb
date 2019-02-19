00e4a42bb61e952aa4c369e52d9aceb59c4b724e
Brandon Hong <byhong@andrew.cmu.edu>
2019-02-18 20:38:22 -0500

    add article and relationships

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ApplicationRecord
+    has_one :category
+    validates_presence_of :title, :content
+    scope :alphabetical, -> { order('title') }
+    scope :active,       -> { where(active: true) }
+
+end
